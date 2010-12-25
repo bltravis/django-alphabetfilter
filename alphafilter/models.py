f25a7be778b76a314fc557b026445aedab02539c
Brooks Travis <brooks.travis@gmail.com>
2010-12-25 03:34:41 -0600

    Adding support for multiple filter_on fields in a given model.

--- alphafilter/models.py
+++ alphafilter/models.py
@@ -30,24 +30,28 @@ class ActiveAlphabet(models.Model):
         return u'Active alphabet for %s on %s' % (self.content_type, self.site)
     
     class Meta:
-        unique_together = (('site', 'content_type'),)
+        unique_together = (('site', 'content_type', 'content_type_field'),)
 
 
 # Signal handler to update active alphabet for a given model
 ## TODO: Use model introspection to determine name for Site foreignkey
 def alpha_check(sender, **kwargs):
     instance = kwargs['instance']
-    active_alphabet = get_object_or_none(ActiveAlphabet, site=Site.objects.get_current(), content_type=ContentType.objects.get_for_model(sender))
-    if active_alphabet:
-        active = active_alphabet.active_alphabet
-        first_letter = instance._meta.get_field(sender.alphafilter_on).value_from_object(instance)[0].upper()
-        if not first_letter in active:
-            new_active = active + first_letter
-            _new_active = list(new_active)
-            new_active = ''.join(_new_active.sort())
-            active_alphabet.active_alphabet = new_active
-            active_alphabet.save()
+    active_alphabets = ActiveAlphabet.objects.filter(site=Site.objects.get_current(), content_type=ContentType.objects.get_for_model(sender))
+    if active_alphabets:
+        for active_alphabet in active_alphabets:
+            for field_name in sender.alphafilter_on:
+                if field_name == active_alphabet.content_type_field:
+                    active = active_alphabet.active_alphabet
+                    first_letter = instance._meta.get_field(field_name).value_from_object(instance)[0].upper()
+                    if not first_letter in active:
+                        new_active = active + first_letter
+                        _new_active = list(new_active)
+                        new_active = ''.join(_new_active.sort())
+                        active_alphabet.active_alphabet = new_active
+                        active_alphabet.save()
      else:
-        active_alphabet = ActiveAlphabet(site=Site.objects.get_current(), content_type=ContentType.objects.get_for_model(sender), content_type_field=sender.alphafilter_on, active_alphabet=u'%s' % (instance._meta.get_field(sender.alphafilter_on)[0].upper(),))
-        active_alphabet.save()
+        for field_name in sender.alphafilter_on:
+            active_alphabet = ActiveAlphabet(site=Site.objects.get_current(), content_type=ContentType.objects.get_for_model(sender), content_type_field=field_name, active_alphabet=u'%s' % (instance._meta.get_field(field_name)[0].upper(),))
+            active_alphabet.save()
     
